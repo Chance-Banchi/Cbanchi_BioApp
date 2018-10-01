97b2d7cc7d39dc1a5143752501781cdaa9f914c3
Chancellor Banchi <cbanchi@gus.pittstate.edu>
2018-10-01 18:02:54 -0500

    Add project files.

---
+++ Cbanchi_BioApp/Cbanchi_BioApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cbanchi_BioApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
