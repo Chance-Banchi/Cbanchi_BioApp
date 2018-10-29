312b9a32b74604a0740eae61d9b94818e20eb7f3
0780878 <0780878@KELCE-ACIS21-P>
2018-10-29 18:34:13 -0500

    Added Switch Contact page Code behind

--- Cbanchi_BioApp/Cbanchi_BioApp/ContactMe.xaml.cs
+++ Cbanchi_BioApp/Cbanchi_BioApp/ContactMe.xaml.cs
@@ -16,5 +16,26 @@ namespace Cbanchi_BioApp
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(nameEntry.Text))
+            {
+                DisplayAlert("Error", "Enter your Name.", "OK!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(emailEntry.Text))
+            {
+                DisplayAlert("Error", "Enter your Email.", "OK!");
+            }
+
+            if (string.IsNullOrEmpty(messageEditor.Text))
+            {
+                DisplayAlert("Error", "Enter your Message.", "OK!");
+            }
+
+            DisplayAlert("Alert", "Your Message Was Sent!", "OK!");
+        }
+    }
 }
